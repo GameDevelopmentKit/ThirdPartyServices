eee21e4ebf61c8a414809a5a2cef69f3dccfcf86
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-02-22 17:47:07 +0700

    Fix firebase tracker when adding firebase sdk

--- ServiceImplementation/FirebaseAnalyticTracker/FirebaseAnalyticTracker.cs
+++ ServiceImplementation/FirebaseAnalyticTracker/FirebaseAnalyticTracker.cs
@@ -6,6 +6,9 @@
     using System.Threading.Tasks;
     using Core.AnalyticServices;
     using Core.AnalyticServices.Data;
+#if FIREBASE_SDK_EXISTS
+    using FirebaseTracker;
+#endif
     using Newtonsoft.Json;
     using UnityEngine;
     using Zenject;
