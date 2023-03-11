718d25bc6de0730253dcb78113212a49a1cd116e
DESKTOP-HE8NS3E\NINH <ninhnv15@gmail.com>
2023-03-11 23:37:58 +0700

    Implement analytic config editor

--- Core/AnalyticServices/AnalyticServicesInstaller.cs
+++ Core/AnalyticServices/AnalyticServicesInstaller.cs
@@ -4,12 +4,14 @@ namespace Core.AnalyticServices
     using Core.AnalyticServices.Signal;
     using Core.AnalyticServices.Tools;
     using GameFoundation.Scripts.Utilities.Extension;
+    using Models;
     using Zenject;
 
     public class AnalyticServicesInstaller : Installer<AnalyticServicesInstaller>
     {
         public override void InstallBindings()
         {
+            this.Container.Bind<AnalyticConfig>().FromResolveGetter<GDKConfig>(config => config.GetGameConfig<AnalyticConfig>()).AsCached();
             this.Container.Bind<IAnalyticServices>().To<AnalyticServices>().AsCached();
             this.Container.Bind<DeviceInfo>().AsCached();
             this.Container.Bind<SessionController>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
