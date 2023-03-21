5864260597185ff64fad0aa2916b07dd2b2b4972
hoanghai145 <hoangthanhhai1308@gmail.com>
2023-03-21 21:32:58 +0700

    Rename IronSource Revenue constants

--- Core/AnalyticServices/CommonEvents/AdsRevenueEvent.cs
+++ Core/AnalyticServices/CommonEvents/AdsRevenueEvent.cs
@@ -13,13 +13,20 @@ namespace Core.AnalyticServices.CommonEvents
         public string Currency;
         public double Revenue;
     }
-    
+
     public class AdRevenueConstants
     {
+        public const string AdjustUrlStrategyChina = "china";
+        public const string AdjustUrlStrategyIndia = "india";
+        public const string AdjustUrlStrategyCn    = "cn";
+        public const string AdjustDataResidencyEU  = "data-residency-eu";
+        public const string AdjustDataResidencyTR  = "data-residency-tr";
+        public const string AdjustDataResidencyUS  = "data-residency-us";
+
         public const string ARSourceAppLovinMAX      = "applovin_max_sdk";
         public const string ARSourceMopub            = "mopub";
         public const string ARSourceAdMob            = "admob_sdk";
-        public const string ARSourceIronSource       = "ironARSourcesdk";
+        public const string ARSourceIronSource       = "ironsource_sdk";
         public const string ARSourceAdmost           = "admost_sdk";
         public const string ARSourceUnity            = "unity_sdk";
         public const string ARSourceHeliumChartboost = "helium_chartboost_sdk";
