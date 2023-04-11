749af4e64e9cc37b1e8421fdf392f9a303650d2b
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-04-11 15:39:28 +0700

    Fix native ads
    Signed-off-by: Tuha <88423816+tuhamm@users.noreply.github.com>

--- Core/AdsServices/Native/NativeAdsView.cs
+++ Core/AdsServices/Native/NativeAdsView.cs
@@ -1,10 +1,14 @@
 namespace Core.AdsServices.Native
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Cysharp.Threading.Tasks;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.Signals;
     using TMPro;
     using UnityEngine;
     using UnityEngine.UI;
+    using Zenject;
 
     public class NativeAdsView : MonoBehaviour
     {
@@ -14,15 +18,35 @@ namespace Core.AdsServices.Native
         public TMP_Text advertiserText;
 
 
+        private SignalBus         signalBus;
         private INativeAdsService nativeAdsService;
+        private List<Type>        activeScreenList;
+        private List<Collider>    colliders;
 
-        public void Init(INativeAdsService nativeAdsService)
+        public void Init(INativeAdsService nativeAdsService, SignalBus signalBus, List<Type> activeScreenList)
         {
+            this.nativeAdsService = nativeAdsService;
+            this.signalBus        = signalBus;
+            this.activeScreenList = activeScreenList;
+
             this.iconImage.gameObject.SetActive(false);
             this.adChoicesImage.gameObject.SetActive(false);
-            this.nativeAdsService = nativeAdsService;
             this.IntervalCall();
+            this.signalBus.Subscribe<ScreenShowSignal>(this.OnScreenShowHandler);
+            this.colliders = this.GetComponentsInChildren<Collider>(true).ToList();
         }
+
+        private void OnDestroy() { this.signalBus.Unsubscribe<ScreenShowSignal>(this.OnScreenShowHandler); }
+
+        private void OnScreenShowHandler(ScreenShowSignal obj)
+        {
+            var isAdsActive = this.activeScreenList.Contains(obj.ScreenPresenter.GetType());
+            foreach (var collider in this.colliders)
+            {
+                collider.enabled = isAdsActive;
+            }
+        }
+
         private async void IntervalCall()
         {
             await UniTask.SwitchToMainThread();
