699229d5ba8e7d2dec686943560781f94ca726e7
hoanghai1308 <hoangthanhhai1308@gmail.com>
2023-04-14 17:59:13 +0700

    update dummy Ads

--- Core/AdsServices/DummyAdServiceIml.cs
+++ Core/AdsServices/DummyAdServiceIml.cs
@@ -2,6 +2,7 @@ namespace Core.AdsServices
 {
     using System;
     using GameFoundation.Scripts.Utilities.LogService;
+    using UnityEngine;
 
     public class DummyAdServiceIml : IAdServices
     {
@@ -9,28 +10,42 @@ namespace Core.AdsServices
 
         public DummyAdServiceIml(ILogService logService) { this.logService = logService; }
 
-        public void GrantDataPrivacyConsent() { this.logService.Log("Dummy Grant consent"); }
-        public void RevokeDataPrivacyConsent() { this.logService.Log("Dummy Revoke consent"); }
-        public void GrantDataPrivacyConsent(AdNetwork adNetwork) { this.logService.Log($"Dummy Grant consent {adNetwork}"); }
-        public void RevokeDataPrivacyConsent(AdNetwork adNetwork) { this.logService.Log($"Dummy revoke consent {adNetwork}"); }
-        public ConsentStatus GetDataPrivacyConsent(AdNetwork adNetwork) { return ConsentStatus.Granted; }
-        public void ShowBannerAd(BannerAdsPosition bannerAdsPosition = BannerAdsPosition.Bottom, int width = 320, int height = 50) { this.logService.Log($"Dummy show banner ad ay {bannerAdsPosition}"); }
-        public void HideBannedAd() { this.logService.Log($"Dummy hide banner ad"); }
-        public void DestroyBannerAd() { this.logService.Log($"Dummy destroy banner ad"); }
+        public void          GrantDataPrivacyConsent()                     { this.logService.Log("Dummy Grant consent"); }
+        public void          RevokeDataPrivacyConsent()                    { this.logService.Log("Dummy Revoke consent"); }
+        public void          GrantDataPrivacyConsent(AdNetwork adNetwork)  { this.logService.Log($"Dummy Grant consent {adNetwork}"); }
+        public void          RevokeDataPrivacyConsent(AdNetwork adNetwork) { this.logService.Log($"Dummy revoke consent {adNetwork}"); }
+        public ConsentStatus GetDataPrivacyConsent(AdNetwork adNetwork)    { return ConsentStatus.Granted; }
+
+        public void ShowBannerAd(BannerAdsPosition bannerAdsPosition = BannerAdsPosition.Bottom, int width = 320, int height = 50)
+        {
+            this.logService.Log($"Dummy show banner ad ay {bannerAdsPosition}");
+        }
+
+        public void HideBannedAd()                      { this.logService.Log($"Dummy hide banner ad"); }
+        public void DestroyBannerAd()                   { this.logService.Log($"Dummy destroy banner ad"); }
         public bool IsInterstitialAdReady(string place) { return true; }
-        public void ShowInterstitialAd(string place) { this.logService.Log($"Dummy show Interstitial ad at {place}"); }
-        public bool IsRewardedAdReady(string place) { return true; }
-        public void ShowRewardedAd(string place) { this.logService.Log($"Dummy show Reward ad at {place}"); }
+        public void ShowInterstitialAd(string place)    { this.logService.Log($"Dummy show Interstitial ad at {place}"); }
+        public bool IsRewardedAdReady(string place)     { return true; }
+        public void ShowRewardedAd(string place)        { this.logService.Log($"Dummy show Reward ad at {place}"); }
+
         public void ShowRewardedAd(string place, Action onCompleted)
         {
             onCompleted?.Invoke();
             this.logService.Log($"Dummy show Reward ad at {place} then do {onCompleted}");
         }
-        public bool IsRewardedInterstitialAdReady() { return true; }
-        public void ShowRewardedInterstitialAd(string place) { this.logService.Log($"Dummy show Rewarded Interstitial ad at {place}"); }
+
+        public bool IsRewardedInterstitialAdReady()                              { return true; }
+        public void ShowRewardedInterstitialAd(string place)                     { this.logService.Log($"Dummy show Rewarded Interstitial ad at {place}"); }
         public void ShowRewardedInterstitialAd(string place, Action onCompleted) { this.logService.Log($"Dummy show Rewarded Interstitial ad at {place} then do {onCompleted}"); }
-        public void RemoveAds(bool revokeConsent = false) { this.logService.Log($"Dummy remove Ads"); }
+
+        public void RemoveAds(bool revokeConsent = false)
+        {
+            PlayerPrefs.SetInt("EM_REMOVE_ADS", -1);
+            this.logService.Log($"Dummy remove Ads");
+        }
+
         public bool IsAdsInitialized() { return true; }
-        public bool IsRemoveAds() { return false; }
+
+        public bool IsRemoveAds() { return PlayerPrefs.HasKey("EM_REMOVE_ADS"); }
     }
 }
