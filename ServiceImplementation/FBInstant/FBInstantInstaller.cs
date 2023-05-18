bce032348e6db5cac97d135d0c5ce01c28dc8580
Manh Nguyen <manhnb1911@gmail.com>
2023-05-18 11:36:06 +0700

    rename assembly 3rd.FBInstant

--- ServiceImplementation/FBInstant/FBInstantInstaller.cs
+++ ServiceImplementation/FBInstant/FBInstantInstaller.cs
@@ -1,17 +1,20 @@
 namespace ServiceImplementation.FBInstant
 {
+#if FB_INSTANT
     using GameFoundation.Scripts.Utilities.UserData;
     using ServiceImplementation.FBInstant.Advertising;
     using ServiceImplementation.FBInstant.EventHandler;
     using ServiceImplementation.FBInstant.Notification;
     using ServiceImplementation.FBInstant.Player;
     using ServiceImplementation.FBInstant.Tournament;
+#endif
     using Zenject;
 
     public class FBInstantInstaller : Installer<FBInstantInstaller>
     {
         public override void InstallBindings()
         {
+#if FB_INSTANT
             this.Container.BindInterfacesAndSelfTo<FBInstantTournament>().AsCached();
             this.Container.BindInterfacesAndSelfTo<FBInstantNotification>().AsCached();
             this.Container.BindInterfacesAndSelfTo<FBEventHandler>().FromNewComponentOnNewGameObject().WithGameObjectName(FBEventHandler.callbackObj).AsCached();
@@ -20,6 +23,7 @@ namespace ServiceImplementation.FBInstant
             this.Container.BindInterfacesAndSelfTo<FBInstantPlayerDataWrapper>().FromNewComponentOnNewGameObject().WithGameObjectName(nameof(FBInstantPlayerDataWrapper)).AsCached();
 
             this.Container.Rebind<IHandleUserDataServices>().To<HandleFBInstantRemoteUserDataServices>().AsCached();
+#endif
         }
     }
 }
