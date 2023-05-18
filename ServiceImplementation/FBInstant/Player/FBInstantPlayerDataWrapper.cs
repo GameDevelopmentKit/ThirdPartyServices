3b708952ef248d755b4cc47ed3b7636abeb819e8
Manh Nguyen <manhnb1911@gmail.com>
2023-05-18 13:41:25 +0700

    change unitask to task

--- ServiceImplementation/FBInstant/Player/FBInstantPlayerDataWrapper.cs
+++ ServiceImplementation/FBInstant/Player/FBInstantPlayerDataWrapper.cs
@@ -2,14 +2,14 @@ namespace ServiceImplementation.FBInstant.Player
 {
     using System;
     using System.Collections.Generic;
-    using Cysharp.Threading.Tasks;
+    using System.Threading.Tasks;
     using Newtonsoft.Json;
     using UnityEngine;
 
     public class FBInstantPlayerDataWrapper : MonoBehaviour
     {
-        private readonly Dictionary<string, UniTaskCompletionSource<string>>           saveUserDataTcs = new();
-        private readonly Dictionary<string, UniTaskCompletionSource<(string, string)>> loadUserDataTcs = new();
+        private readonly Dictionary<string, TaskCompletionSource<string>>           saveUserDataTcs = new();
+        private readonly Dictionary<string, TaskCompletionSource<(string, string)>> loadUserDataTcs = new();
 
         private void OnUserDataSaved(string message)
         {
@@ -27,7 +27,8 @@ namespace ServiceImplementation.FBInstant.Player
 
             FBInstantPlayer.SaveUserData(key, json, this.gameObject.name, nameof(this.OnUserDataSaved), id);
 
-            var error = UniTask.RunOnThreadPool(() => this.saveUserDataTcs[id].Task).GetAwaiter().GetResult();
+            // var error = Task.Run(() => this.saveUserDataTcs[id].Task).Result;
+            var error = Task.Run(() => this.saveUserDataTcs[id].Task).ConfigureAwait(false).GetAwaiter().GetResult();
             this.saveUserDataTcs.Remove(id);
 
             if (error is not null)
@@ -53,7 +54,8 @@ namespace ServiceImplementation.FBInstant.Player
 
             FBInstantPlayer.LoadUserData(key, this.gameObject.name, nameof(this.OnUserDataLoaded), id);
 
-            var (data, error) = UniTask.RunOnThreadPool(() => this.loadUserDataTcs[id].Task).GetAwaiter().GetResult();
+            // var (data, error) = Task.Run(() => this.loadUserDataTcs[id].Task).Result;
+            var (data, error) = Task.Run(() => this.loadUserDataTcs[id].Task).ConfigureAwait(false).GetAwaiter().GetResult();
             this.loadUserDataTcs.Remove(id);
 
             if (error is not null)
