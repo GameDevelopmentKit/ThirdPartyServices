dd4a7383c3ebbdb4decdbcdd631c12b6df38af26
Nguyễn Mạnh <75465287+frostbun@users.noreply.github.com>
2023-05-22 16:41:22 +0700

    fix fb instant save user data (#41)

--- ServiceImplementation/FBInstant/Player/HandleFBInstantRemoteUserDataServices.cs
+++ ServiceImplementation/FBInstant/Player/HandleFBInstantRemoteUserDataServices.cs
@@ -1,10 +1,8 @@
 namespace ServiceImplementation.FBInstant.Player
 {
-    using System.Linq;
     using Cysharp.Threading.Tasks;
     using GameFoundation.Scripts.Utilities.LogService;
     using GameFoundation.Scripts.Utilities.UserData;
-    using Newtonsoft.Json;
 
     public class HandleFBInstantRemoteUserDataServices : BaseHandleUserDataServices
     {
@@ -17,7 +15,7 @@ namespace ServiceImplementation.FBInstant.Player
 
         protected override UniTask SaveJsons(params (string key, string json)[] values)
         {
-            return this.fbInstantPlayer.SaveUserData(JsonConvert.SerializeObject(values.ToDictionary(value => value.key, value => value.json)));
+            return this.fbInstantPlayer.SaveUserData(values);
         }
 
         protected override UniTask<string[]> LoadJsons(params string[] keys)
