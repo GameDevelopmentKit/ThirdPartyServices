f4bd10dac9ae68caa82d96241edb0306c0b5e691
Quan Hoang <qmhoang94@gmail.com>
2023-05-25 01:40:02 +0700

    base64string

--- ServiceImplementation/FBInstant/Notification/FBInstantNotification.cs
+++ ServiceImplementation/FBInstant/Notification/FBInstantNotification.cs
@@ -27,8 +27,9 @@ namespace ServiceImplementation.FBInstant.Notification
 
         public static void FacebookInstantSendInvite(Sprite img, string text, string localizationJson)
         {
-            var imgBase64 = img.texture.EncodeToPNG();
-            fbinstant_inviteAsync(Convert.ToBase64String(imgBase64), text, localizationJson, FBEventHandler.callbackObj, nameof(FacebookInstantSendInviteCallback));
+            byte[] imgBase64 = img.texture.EncodeToPNG();
+            string base64String = Convert.ToBase64String(imgBase64);
+            fbinstant_inviteAsync(base64String, text, localizationJson, FBEventHandler.callbackObj, nameof(FacebookInstantSendInviteCallback));
         }
 
         public void FacebookInstantSendInviteCallback()
