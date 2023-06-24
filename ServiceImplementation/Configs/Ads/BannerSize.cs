3433fd8ee6cc7e5b6e50187d22e115c987d8d018
DuyMaiBui` <duymaibui2k@gmail.com>
2023-06-25 00:08:23 +0700

    fix error build

--- ServiceImplementation/Configs/Ads/BannerSize.cs
+++ ServiceImplementation/Configs/Ads/BannerSize.cs
@@ -10,6 +10,18 @@ namespace ServiceImplementation.Configs.Ads
             this.height = height;
         }
 
+        public override bool Equals(object obj)
+        {
+            var other = obj as BannerSize;
+
+            if (other == null)
+                return false;
+
+            return this.width.Equals(other.width) && this.height.Equals(other.height);
+        }
+
+        public override int GetHashCode() { return this.width.GetHashCode(); }
+        
         public static bool operator ==(BannerSize bannerSize1, BannerSize bannerSize2) { return bannerSize1?.Equals(bannerSize2) ?? ReferenceEquals(bannerSize2, null); }
 
         public static bool operator !=(BannerSize bannerSize1, BannerSize bannerSize2) { return !(bannerSize1 == bannerSize2); }
