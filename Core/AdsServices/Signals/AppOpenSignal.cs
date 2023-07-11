5de0a20cbed27a58e569548e174313cee0a2b77a
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-07-11 19:05:42 +0700

    ImpImplement AOA signals

--- Core/AdsServices/Signals/AppOpenSignal.cs
+++ Core/AdsServices/Signals/AppOpenSignal.cs
@@ -6,9 +6,45 @@ namespace Core.AdsServices.Signals
        {
        }
    }
+   
+   public class AppOpenFullScreenContentFailedSignal : BaseAdsSignal
+   {
+       public AppOpenFullScreenContentFailedSignal(string placement) : base(placement)
+       {
+       }
+   }
+   
    public class AppOpenFullScreenContentClosedSignal : BaseAdsSignal
    {
        public AppOpenFullScreenContentClosedSignal(string placement) : base(placement)
+       {
+       }
+   }
+   
+   public class AppOpenLoadedSignal : BaseAdsSignal
+   {
+       public AppOpenLoadedSignal(string placement) : base(placement)
+       {
+       }
+   }
+   
+   public class AppOpenLoadFailedSignal : BaseAdsSignal
+   {
+       public AppOpenLoadFailedSignal(string placement) : base(placement)
+       {
+       }
+   }
+   
+   public class AppOpenEligibleSignal : BaseAdsSignal
+   {
+       public AppOpenEligibleSignal(string placement) : base(placement)
+       {
+       }
+   }
+   
+   public class AppOpenCalledSignal : BaseAdsSignal
+   {
+       public AppOpenCalledSignal(string placement) : base(placement)
        {
        }
    }
