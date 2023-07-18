9fd45757fee1bdec1118142502a10931695816e7
zommpie-duymai <buiduymai2k@gmail.com>
2023-07-18 18:43:27 +0700

    remove RemoveAdsService

--- ServiceImplementation/IAPServices/IapInstaller.cs
+++ ServiceImplementation/IAPServices/IapInstaller.cs
@@ -1,6 +1,5 @@
 ﻿namespace ServiceImplementation.IAPServices
 {
-    using System.Collections.Generic;
     using GameFoundation.Scripts.Utilities.LogService;
     using UnityEngine;
     using Zenject;
@@ -14,13 +13,8 @@
             this.Container.Bind<IRemoveAdsServices>().To<DummyRemoveAdsIapServices>().AsCached().NonLazy();
 #else
             this.Container.Bind<IIapServices>().To<UnityIapServices>().AsCached().NonLazy();
-            this.Container.Bind<IRemoveAdsServices>().To<RemoveAdsIapServices>().AsCached().NonLazy();
             this.Container.Resolve<ILogService>().LogWithColor("IAP Enable, don't forget to call IIapServices.InitIapServices in your game,ignore if already done!!", Color.red);
 #endif
-            this.Container.Bind<RemoveAdData>().FromInstance(new RemoveAdData()
-            {
-                listIdRemoveAds = new List<string>() { "RemoveAds" }
-            }).AsCached().NonLazy();
 
             this.Container.DeclareSignal<RemoveAdsCompleteSignal>();
             this.Container.DeclareSignal<OnRestorePurchaseCompleteSignal>();
