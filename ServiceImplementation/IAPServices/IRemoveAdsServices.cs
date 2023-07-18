17c7be0288281d89c96c9d935e10eac7ee52f265
zommpie-duymai <buiduymai2k@gmail.com>
2023-07-18 18:43:27 +0700

    remove RemoveAdsService

--- ServiceImplementation/IAPServices/IRemoveAdsServices.cs
+++ ServiceImplementation/IAPServices/IRemoveAdsServices.cs
@@ -1,12 +1,5 @@
 ﻿namespace ServiceImplementation.IAPServices
 {
-    using System;
-
-    public interface IRemoveAdsServices
-    {
-        void BuyRemoveAds(string removeAdsId, Action<string> onComplete = null, Action<string> onFailed = null);
-    }
-
     public class RemoveAdsCompleteSignal
     {
     }
