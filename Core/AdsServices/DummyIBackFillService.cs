c6ea9068e67faf06c388006464c588ccb1fcc926
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-08-08 13:47:55 +0700

    update backfill adservice

---
+++ Core/AdsServices/DummyIBackFillService.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.AdsServices
+{
+    public class DummyIBackFillService : IBackFillAdsService
+    {
+        public bool IsInterstitialAdReady(string place) => false;
+        public void ShowInterstitialAd(string place)    { }
+        public void LoadInterstitialAd(string place)    { }
+    }
+}
