4665eb60489f80685d7edee7392149a0c6054497
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-10-24 12:06:20 +0700

    Implement remote config mapping

---
+++ ServiceImplementation/Configs/Ads/AdServicesConfig.cs
@@ -0,0 +1,60 @@
+namespace Core.AdsServices
+{
+    using System;
+    using ServiceImplementation.FireBaseRemoteConfig;
+    using Zenject;
+
+    public class AdServicesConfig : IInitializable, IDisposable
+    {
+        #region inject
+
+        private readonly SignalBus           signalBus;
+        private readonly IRemoteConfig       remoteConfig;
+        private readonly RemoteConfigSetting remoteConfigSetting;
+
+        #endregion
+        
+        public bool EnableBannerAd               { get; set; } = true;
+        public bool EnableInterstitialAd         { get; set; } = true;
+        public bool EnableMRECAd                 { get; set; } = true;
+        public bool EnableAOAAd                  { get; set; } = true;
+        public bool EnableRewardedAd             { get; set; } = true;
+        public bool EnableRewardedInterstitialAd { get; set; } = true;
+        public bool EnableNativeAd               { get; set; } = true;
+        public int  IntervalLoadAds              { get; set; } = 5;
+
+        public int InterstitialAdInterval    { get; set; } = 15;
+        public int MinPauseSecondToShowAoaAd { get; set; } = 0;
+
+        public AdServicesConfig(SignalBus signalBus, IRemoteConfig remoteConfig, RemoteConfigSetting remoteConfigSetting)
+        {
+            this.signalBus            = signalBus;
+            this.remoteConfig         = remoteConfig;
+            this.remoteConfigSetting = remoteConfigSetting;
+        }
+        
+        public void Initialize()
+        {
+            this.signalBus.Subscribe<RemoteConfigFetchedSucceededSignal>(this.RemoteConfigFetchedSucceededHandler);
+        }
+        
+        public void Dispose()
+        {
+            this.signalBus.Unsubscribe<RemoteConfigFetchedSucceededSignal>(this.RemoteConfigFetchedSucceededHandler);
+        }
+
+        private void RemoteConfigFetchedSucceededHandler()
+        {
+            this.EnableBannerAd               = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_banner_ad"), true);
+            this.EnableInterstitialAd         = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_interstitial_ad"), true);
+            this.EnableMRECAd                 = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_mrec_ad"), true);
+            this.EnableAOAAd                  = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_aoa_ad"), true);
+            this.EnableRewardedAd             = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_rewarded_ad"), true);
+            this.EnableRewardedInterstitialAd = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_rewarded_interstitial_ad"), true);
+            this.EnableNativeAd               = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_native_ad"), true);
+            this.IntervalLoadAds              = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("interval_load_ads"), 5);
+            this.InterstitialAdInterval       = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("interstitial_ad_interval"), 15);
+            this.MinPauseSecondToShowAoaAd    = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("min_pause_second_to_show_aoa_ad"), 0);
+        }
+    }
+}
---
+++ ServiceImplementation/Configs/RemoteConfig/RemoteConfigSetting.cs
@@ -0,0 +1,31 @@
+namespace ServiceImplementation.FireBaseRemoteConfig
+{
+    using ServiceImplementation.Configs.Common;
+    using Sirenix.OdinInspector;
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = nameof(RemoteConfigSetting), menuName = "ScriptableObjects/SpawnRemoteMappingConfig", order = 1)]
+    public class RemoteConfigSetting : ScriptableObject
+    {
+        public static string ResourcePath = $"GameConfigs/{nameof(RemoteConfigSetting)}";
+
+        public StringStringSerializableDictionary RemoteKeyMapping => this.mRemoteKeyMapping;
+
+        [SerializeField] [LabelText("Remove Config Mapping")]
+        private StringStringSerializableDictionary mRemoteKeyMapping = new()
+        {
+            { "enable_banner_ad", "enable_banner_ad" },
+            { "enable_interstitial_ad", "enable_interstitial_ad" },
+            { "enable_mrec_ad", "enable_mrec_ad" },
+            { "enable_aoa_ad", "enable_aoa_ad" },
+            { "enable_rewarded_ad", "enable_rewarded_ad" },
+            { "enable_rewarded_interstitial_ad", "enable_rewarded_interstitial_ad" },
+            { "enable_native_ad", "enable_native_ad" },
+            { "interval_load_ads", "interval_load_ads" },
+            { "interstitial_ad_interval", "interstitial_ad_interval" },
+            { "min_pause_second_to_show_aoa_ad", "min_pause_second_to_show_aoa_ad" }
+        };
+        
+        public string GetRemoteKey(string key) => this.mRemoteKeyMapping.TryGetValue(key, out var remoteKey) ? remoteKey : key;
+    }
+}
