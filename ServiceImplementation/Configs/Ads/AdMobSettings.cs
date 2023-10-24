4e093080aa78238568f745cf1c75e57cfb93a10b
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-10-24 14:20:44 +0700

    Move Admob setting to theone window

--- ServiceImplementation/Configs/Ads/AdMobSettings.cs
+++ ServiceImplementation/Configs/Ads/AdMobSettings.cs
@@ -2,10 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Reflection;
     using ServiceImplementation.Configs.Common;
     using Sirenix.OdinInspector;
     using UnityEngine;
-    using UnityEngine.Serialization;
+    using Object = UnityEngine.Object;
 
     [Serializable]
     public class AdMobSettings : AdNetworkSettings
@@ -29,17 +30,17 @@
         /// Gets or sets the default rewarded interstitial ad identifier.
         /// </summary>
         public AdId DefaultRewardedInterstitialAdId { get => this.mDefaultRewardedInterstitialAdId; set => this.mDefaultRewardedInterstitialAdId = value; }
-        
+
         /// <summary>
         /// Gets or sets the default AOA ad identifier.
         /// </summary>
         public AdId AOAAdId { get => this.mAoaAdId; set => this.mAoaAdId = value; }
-        
+
         /// <summary>
         /// Gets or sets the default native ad identifier.
         /// </summary>
         public List<AdId> NativeAdIds { get => this.mNativeAdIds; set => this.mNativeAdIds = value; }
-        
+
         /// <summary>
         /// Gets or sets the default MREC ad identifier.
         /// </summary>
@@ -78,6 +79,63 @@
             set => this.mCustomRewardedInterstitialAdIds = value as Dictionary_AdPlacement_AdId;
         }
 
+        [OnInspectorInit]
+        private void LoadAdmobSetting()
+        {
+            var googleMobileAdsSettings = Resources.Load<Object>("GoogleMobileAdsSettings");
+
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var settingType  = googleMobileAdsSettings.GetType();
+
+            this.mIOSAppId     = settingType.GetField("adMobIOSAppId", bindingFlags).GetValue(googleMobileAdsSettings) as string;
+            this.mAndroidAppId = settingType.GetField("adMobAndroidAppId", bindingFlags).GetValue(googleMobileAdsSettings) as string;
+
+            this.mOptimizeInitialization = (bool)settingType.GetField("optimizeInitialization", bindingFlags).GetValue(googleMobileAdsSettings);
+
+            this.mOptimizeAdLoading = (bool)settingType.GetField("optimizeAdLoading", bindingFlags).GetValue(googleMobileAdsSettings);
+
+            this.mDelayAppMeasurementInit = (bool)settingType.GetField("delayAppMeasurementInit", bindingFlags).GetValue(googleMobileAdsSettings);
+
+            this.mUserTrackingUsageDescription = settingType.GetField("userTrackingUsageDescription", bindingFlags).GetValue(googleMobileAdsSettings) as string;
+        }
+
+        private void SaveAdmobSetting()
+        {
+            var googleMobileAdsSettings = Resources.Load<Object>("GoogleMobileAdsSettings");
+
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var settingType  = googleMobileAdsSettings.GetType();
+
+            settingType.GetField("adMobIOSAppId", bindingFlags).SetValue(googleMobileAdsSettings, this.mIOSAppId);
+            settingType.GetField("adMobAndroidAppId", bindingFlags).SetValue(googleMobileAdsSettings, this.mAndroidAppId);
+            settingType.GetField("optimizeInitialization", bindingFlags).SetValue(googleMobileAdsSettings, this.mOptimizeInitialization);
+            settingType.GetField("optimizeAdLoading", bindingFlags).SetValue(googleMobileAdsSettings, this.mOptimizeAdLoading);
+            settingType.GetField("delayAppMeasurementInit", bindingFlags).SetValue(googleMobileAdsSettings, this.mDelayAppMeasurementInit);
+            settingType.GetField("userTrackingUsageDescription", bindingFlags).SetValue(googleMobileAdsSettings, this.mUserTrackingUsageDescription);
+        }
+
+        private void AppIdChanged() { Debug.Log("Admob app id changed"); }
+
+        [OnValueChanged("SaveAdmobSetting")] [Header("Google Mobile Ads App ID")] [BoxGroup("Admob Settings")] [SerializeField] [LabelText("Android App Id")]
+        private string mAndroidAppId;
+
+        [OnValueChanged("SaveAdmobSetting")] [BoxGroup("Admob Settings")] [SerializeField] [LabelText("IOS App Id")]
+        private string mIOSAppId;
+
+
+        [OnValueChanged("SaveAdmobSetting")] [Header("Android optimization settings")] [BoxGroup("Admob Settings")] [SerializeField] [LabelText("Optimize initialization")]
+        private bool mOptimizeInitialization;
+
+        [OnValueChanged("SaveAdmobSetting")] [BoxGroup("Admob Settings")] [SerializeField] [LabelText("Optimize ad loading")]
+        private bool mOptimizeAdLoading;
+
+        [OnValueChanged("SaveAdmobSetting")] [Header("Admob-specific settings")] [SerializeField] [BoxGroup("Admob Settings")]
+        private bool mDelayAppMeasurementInit;
+
+        [OnValueChanged("SaveAdmobSetting")] [Header("UMP-specific settings")] [SerializeField] [BoxGroup("Admob Settings")]
+        private string mUserTrackingUsageDescription;
+
+
         [SerializeField] [LabelText("Enable Test Mode")]
         private bool mEnableTestMode;
 
@@ -92,10 +150,10 @@
 
         [SerializeField] [LabelText("Rewarded Interstitial")] [BoxGroup("Default Id")]
         private AdId mDefaultRewardedInterstitialAdId;
-        
+
         [SerializeField] [LabelText("AOA")] [BoxGroup("Default Id")]
         private AdId mAoaAdId;
-        
+
         [SerializeField] [LabelText("Native")] [BoxGroup("Default Id")]
         private List<AdId> mNativeAdIds;
 
