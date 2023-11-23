c542772f3cb10b812659f9ad186409316071d870
phuongdmp7 <83566201+phuongdmp7@users.noreply.github.com>
2023-11-23 16:31:25 +0700

    add default remote config value (#93)
    * add default remote config value
    
    * update

--- ServiceImplementation/Configs/Ads/AdServicesConfig.cs
+++ ServiceImplementation/Configs/Ads/AdServicesConfig.cs
@@ -13,61 +13,73 @@ namespace ServiceImplementation.Configs.Ads
         private readonly RemoteConfigSetting remoteConfigSetting;
 
         #endregion
-        
-        //General setting
-        public int  IntervalLoadAds              { get; set; } = 5;
-        public bool EnableInterstitialAd         { get; set; } = true;
-        public bool EnableBannerAd               { get; set; } = true;
-        public bool EnableMRECAd                 { get; set; } = true;
-        public bool EnableAOAAd                  { get; set; } = true;
-        public bool EnableRewardedAd             { get; set; } = true;
-        public bool EnableNativeAd               { get; set; } = true;
-        public bool EnableRewardedInterstitialAd { get; set; } = true;
 
+        //General setting
+        public int  IntervalLoadAds              { get; set; }
+        public bool EnableInterstitialAd         { get; set; }
+        public bool EnableBannerAd               { get; set; }
+        public bool EnableMRECAd                 { get; set; }
+        public bool EnableAOAAd                  { get; set; }
+        public bool EnableRewardedAd             { get; set; }
+        public bool EnableNativeAd               { get; set; }
+        public bool EnableRewardedInterstitialAd { get; set; }
 
         //Interstitial ads
-        public int  InterstitialAdInterval           { get; set; } = 15; //The interval between two interstitial ads, we also count the rewarded interstitial ads
-        public int  DelayFirstInterstitialAdInterval { get; set; } = 0; //This delay will be applied for the first session
-        public int  DelayFirstInterNewSession        { get; set; } = 0; //From the second session, this delay will be applied
-        public int  InterstitialAdStartLevel         { get; set; } = 1; //The level to start showing interstitial ads
-
+        public int InterstitialAdInterval           { get; set; } //The interval between two interstitial ads, we also count the rewarded interstitial ads
+        public int DelayFirstInterstitialAdInterval { get; set; } //This delay will be applied for the first session
+        public int DelayFirstInterNewSession        { get; set; } //From the second session, this delay will be applied
+        public int InterstitialAdStartLevel         { get; set; } //The level to start showing interstitial ads
 
         //AOA
-        public int MinPauseSecondToShowAoaAd { get; set; } = 0;
-        public int AOAStartSession           { get; set; } = 2;
+        public int MinPauseSecondToShowAoaAd { get; set; }
+        public int AOAStartSession           { get; set; }
 
         public AdServicesConfig(SignalBus signalBus, IRemoteConfig remoteConfig, RemoteConfigSetting remoteConfigSetting)
         {
             this.signalBus           = signalBus;
             this.remoteConfig        = remoteConfig;
             this.remoteConfigSetting = remoteConfigSetting;
+
+            // Init default value
+            this.OnRemoteConfigFetchedSucceeded();
         }
 
-        public void Initialize() { this.signalBus.Subscribe<RemoteConfigFetchedSucceededSignal>(this.RemoteConfigFetchedSucceededHandler); }
+        public void Initialize() { this.signalBus.Subscribe<RemoteConfigFetchedSucceededSignal>(this.OnRemoteConfigFetchedSucceeded); }
 
-        public void Dispose() { this.signalBus.Unsubscribe<RemoteConfigFetchedSucceededSignal>(this.RemoteConfigFetchedSucceededHandler); }
+        public void Dispose() { this.signalBus.Unsubscribe<RemoteConfigFetchedSucceededSignal>(this.OnRemoteConfigFetchedSucceeded); }
 
-        private void RemoteConfigFetchedSucceededHandler()
+        private void OnRemoteConfigFetchedSucceeded()
         {
             //General setting
-            this.IntervalLoadAds                  = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("interval_load_ads"), 5);
-            this.EnableBannerAd                   = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_banner_ad"), true);
-            this.EnableMRECAd                     = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_mrec_ad"), true);
-            this.EnableRewardedAd                 = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_rewarded_ad"), true);
-            this.EnableNativeAd                   = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_native_ad"), true);
-            this.EnableInterstitialAd             = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_interstitial_ad"), true);
-            
-            //Interstitial
-            this.EnableRewardedInterstitialAd     = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_rewarded_interstitial_ad"), true);
-            this.InterstitialAdInterval           = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("interstitial_ad_interval"), 15);
-            this.DelayFirstInterstitialAdInterval = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("delay_first_inters_ad_interval"), 0);
-            this.DelayFirstInterNewSession        = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("delay_first_inters_new_session"), 0);
-            this.InterstitialAdStartLevel         = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("interstitial_ad_start_level"), 1);
-            
-            //AOA
-            this.EnableAOAAd                      = this.remoteConfig.GetRemoteConfigBoolValue(this.remoteConfigSetting.GetRemoteKey("enable_aoa_ad"), true);
-            this.MinPauseSecondToShowAoaAd        = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("min_pause_second_to_show_aoa_ad"), 0);
-            this.AOAStartSession                  = this.remoteConfig.GetRemoteConfigIntValue(this.remoteConfigSetting.GetRemoteKey("aoa_start_session"), 2);
+            var intervalLoadAdsSetting              = this.remoteConfigSetting.GetRemoteConfig("interval_load_ads");
+            var enableBannerADSetting               = this.remoteConfigSetting.GetRemoteConfig("enable_banner_ad");
+            var enableMrecADSetting                 = this.remoteConfigSetting.GetRemoteConfig("enable_mrec_ad");
+            var enableRewardedADSetting             = this.remoteConfigSetting.GetRemoteConfig("enable_rewarded_ad");
+            var enableNativeADSetting               = this.remoteConfigSetting.GetRemoteConfig("enable_native_ad");
+            var enableInterstitialADSetting         = this.remoteConfigSetting.GetRemoteConfig("enable_interstitial_ad");
+            var enableRewardedInterstitialADSetting = this.remoteConfigSetting.GetRemoteConfig("enable_rewarded_interstitial_ad");
+            var interstitialADIntervalSetting       = this.remoteConfigSetting.GetRemoteConfig("interstitial_ad_interval");
+            var delayFirstIntersADIntervalSetting   = this.remoteConfigSetting.GetRemoteConfig("delay_first_inters_ad_interval");
+            var delayFirstIntersNewSessionSetting   = this.remoteConfigSetting.GetRemoteConfig("delay_first_inters_new_session");
+            var interstitialADStartLevelSetting     = this.remoteConfigSetting.GetRemoteConfig("interstitial_ad_start_level");
+            var enableAoaADSetting                  = this.remoteConfigSetting.GetRemoteConfig("enable_aoa_ad");
+            var minPauseSecondToShowAoaADSetting    = this.remoteConfigSetting.GetRemoteConfig("min_pause_second_to_show_aoa_ad");
+            var aoaStartSessionSetting              = this.remoteConfigSetting.GetRemoteConfig("aoa_start_session");
+
+            this.IntervalLoadAds                  = this.remoteConfig.GetRemoteConfigIntValue(intervalLoadAdsSetting.key, int.Parse(intervalLoadAdsSetting.defaultValue));
+            this.EnableBannerAd                   = this.remoteConfig.GetRemoteConfigBoolValue(enableBannerADSetting.key, bool.Parse(enableBannerADSetting.defaultValue));
+            this.EnableMRECAd                     = this.remoteConfig.GetRemoteConfigBoolValue(enableMrecADSetting.key, bool.Parse(enableMrecADSetting.defaultValue));
+            this.EnableRewardedAd                 = this.remoteConfig.GetRemoteConfigBoolValue(enableRewardedADSetting.key, bool.Parse(enableRewardedADSetting.defaultValue));
+            this.EnableNativeAd                   = this.remoteConfig.GetRemoteConfigBoolValue(enableNativeADSetting.key, bool.Parse(enableNativeADSetting.defaultValue));
+            this.EnableInterstitialAd             = this.remoteConfig.GetRemoteConfigBoolValue(enableInterstitialADSetting.key, bool.Parse(enableInterstitialADSetting.defaultValue));
+            this.EnableRewardedInterstitialAd     = this.remoteConfig.GetRemoteConfigBoolValue(enableRewardedInterstitialADSetting.key, bool.Parse(enableRewardedInterstitialADSetting.defaultValue));
+            this.InterstitialAdInterval           = this.remoteConfig.GetRemoteConfigIntValue(interstitialADIntervalSetting.key, int.Parse(interstitialADIntervalSetting.defaultValue));
+            this.DelayFirstInterstitialAdInterval = this.remoteConfig.GetRemoteConfigIntValue(delayFirstIntersADIntervalSetting.key, int.Parse(delayFirstIntersADIntervalSetting.defaultValue));
+            this.DelayFirstInterNewSession        = this.remoteConfig.GetRemoteConfigIntValue(delayFirstIntersNewSessionSetting.key, int.Parse(delayFirstIntersNewSessionSetting.defaultValue));
+            this.InterstitialAdStartLevel         = this.remoteConfig.GetRemoteConfigIntValue(interstitialADStartLevelSetting.key, int.Parse(interstitialADStartLevelSetting.defaultValue));
+            this.EnableAOAAd                      = this.remoteConfig.GetRemoteConfigBoolValue(enableAoaADSetting.key, bool.Parse(enableAoaADSetting.defaultValue));
+            this.MinPauseSecondToShowAoaAd        = this.remoteConfig.GetRemoteConfigIntValue(minPauseSecondToShowAoaADSetting.key, int.Parse(minPauseSecondToShowAoaADSetting.defaultValue));
+            this.AOAStartSession                  = this.remoteConfig.GetRemoteConfigIntValue(aoaStartSessionSetting.key, int.Parse(aoaStartSessionSetting.defaultValue));
         }
     }
 }
---
+++ ServiceImplementation/Configs/RemoteConfig/RemoteConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace ServiceImplementation.FireBaseRemoteConfig
+{
+    using System;
+
+    [Serializable]
+    public class RemoteConfig
+    {
+        public string key;
+        public string mapping;
+        public string defaultValue;
+
+        public RemoteConfig(string key, string mapping, string defaultValue)
+        {
+            this.key          = key;
+            this.mapping      = mapping;
+            this.defaultValue = defaultValue;
+        }
+    }
+}
