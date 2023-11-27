be3b32ad83364e491fec3035ffe6030464e5b065
phuongdmp7 <83566201+phuongdmp7@users.noreply.github.com>
2023-11-27 20:23:32 +0700

    Phuongdmp/native/update remote config (#94)
    * update remote config
    
    * update cross platform remote id

--- ServiceImplementation/Configs/RemoteConfig/RemoteConfig.cs
+++ ServiceImplementation/Configs/RemoteConfig/RemoteConfig.cs
@@ -1,19 +1,20 @@
 ﻿namespace ServiceImplementation.FireBaseRemoteConfig
 {
     using System;
+    using ServiceImplementation.Configs.Ads;
 
     [Serializable]
     public class RemoteConfig
     {
         public string key;
-        public string mapping;
-        public string defaultValue;
+        public AdId   mapping;
+        public AdId   defaultValue;
 
         public RemoteConfig(string key, string mapping, string defaultValue)
         {
             this.key          = key;
-            this.mapping      = mapping;
-            this.defaultValue = defaultValue;
+            this.mapping      = new AdId(mapping, mapping);
+            this.defaultValue = new AdId(defaultValue, defaultValue);
         }
     }
 }
