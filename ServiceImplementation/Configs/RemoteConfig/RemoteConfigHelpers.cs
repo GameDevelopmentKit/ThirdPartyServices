bbed2b79da8e751fde5bd9007092629a74ce89f8
phuongdmp7 <phuongdmp7@gmail.com>
2023-11-27 19:14:05 +0700

    update remote config

---
+++ ServiceImplementation/Configs/RemoteConfig/RemoteConfigHelpers.cs
@@ -0,0 +1,94 @@
+﻿namespace ServiceImplementation.FireBaseRemoteConfig
+{
+    using UnityEngine;
+
+    public static class RemoteConfigHelpers
+    {
+        #region Remote Value
+
+        public static int GetIntRemoteValue(IRemoteConfig remoteConfig, RemoteConfigSetting remoteConfigSetting, string key)
+        {
+            var config = remoteConfigSetting.GetRemoteConfig(key);
+            return remoteConfig.GetRemoteConfigIntValue(config.mapping, GetIntDefaultValue(config));
+        }
+
+        public static bool GetBoolRemoteValue(IRemoteConfig remoteConfig, RemoteConfigSetting remoteConfigSetting, string key)
+        {
+            var config = remoteConfigSetting.GetRemoteConfig(key);
+            return remoteConfig.GetRemoteConfigBoolValue(config.mapping, GetBoolDefaultValue(config));
+        }
+
+        public static float GetFloatRemoteValue(IRemoteConfig remoteConfig, RemoteConfigSetting remoteConfigSetting, string key)
+        {
+            var config = remoteConfigSetting.GetRemoteConfig(key);
+            return remoteConfig.GetRemoteConfigFloatValue(config.mapping, GetFloatDefaultValue(config));
+        }
+
+        public static string GetStringRemoteValue(IRemoteConfig remoteConfig, RemoteConfigSetting remoteConfigSetting, string key)
+        {
+            var config = remoteConfigSetting.GetRemoteConfig(key);
+            return remoteConfig.GetRemoteConfigStringValue(config.mapping, config.defaultValue);
+        }
+
+        #endregion
+
+        #region Default Value
+
+        public static int GetIntDefaultValue(RemoteConfigSetting remoteConfigSetting, string key)
+        {
+            var config = remoteConfigSetting.GetRemoteConfig(key);
+            return GetIntDefaultValue(config);
+        }
+
+        public static bool GetBoolDefaultValue(RemoteConfigSetting remoteConfigSetting, string key)
+        {
+            var config = remoteConfigSetting.GetRemoteConfig(key);
+            return GetBoolDefaultValue(config);
+        }
+
+        public static float GetFloatDefaultValue(RemoteConfigSetting remoteConfigSetting, string key)
+        {
+            var config = remoteConfigSetting.GetRemoteConfig(key);
+            return GetFloatDefaultValue(config);
+        }
+
+        #endregion
+
+        #region Get Default Value
+
+        private static int GetIntDefaultValue(RemoteConfig config)
+        {
+            if (int.TryParse(config.defaultValue, out var result))
+            {
+                return result;
+            }
+
+            Debug.LogError($"Can not parse int value from remote config key: {config.key}");
+            return 0;
+        }
+
+        private static bool GetBoolDefaultValue(RemoteConfig config)
+        {
+            if (bool.TryParse(config.defaultValue, out var result))
+            {
+                return result;
+            }
+
+            Debug.LogError($"Can not parse bool value from remote config key: {config.key}");
+            return false;
+        }
+
+        private static float GetFloatDefaultValue(RemoteConfig config)
+        {
+            if (float.TryParse(config.defaultValue, out var result))
+            {
+                return result;
+            }
+
+            Debug.LogError($"Can not parse float value from remote config key: {config.key}");
+            return 0;
+        }
+
+        #endregion
+    }
+}
