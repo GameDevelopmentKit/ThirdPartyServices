48c3ca7490b7b92f211fca8fcd37c6da89200ffd
phuongdmp7 <phuongdmp7@gmail.com>
2024-01-30 17:01:40 +0700

    change mechanic colapsible banner ad interval

--- ServiceImplementation/Configs/RemoteConfig/RemoteConfigKey.cs
+++ ServiceImplementation/Configs/RemoteConfig/RemoteConfigKey.cs
@@ -19,6 +19,6 @@
         public const string DelayFirstIntersADInterval        = "delay_first_inters_ad_interval";
         public const string DelayFirstIntersNewSession        = "delay_first_inters_new_session";
         public const string ResetInterAdIntervalAfterRewardAd = "reset_inter_ad_interval_after_reward_ad";
-        public const string BannerADInterval                  = "banner_ad_interval";
+        public const string CollapsibleBannerADInterval       = "colapsible_banner_ad_interval";
     }
 }
