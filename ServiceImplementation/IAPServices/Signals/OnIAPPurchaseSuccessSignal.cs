cc9902cf23c11472f43ee2bb46095e6ac5999ec1
ducyb294 <ducyb2942@gmail.com>
2024-03-04 15:10:01 +0700

    add THEONE_IAP symbol

--- ServiceImplementation/IAPServices/Signals/OnIAPPurchaseSuccessSignal.cs
+++ ServiceImplementation/IAPServices/Signals/OnIAPPurchaseSuccessSignal.cs
@@ -1,10 +1,14 @@
 ﻿namespace ServiceImplementation.IAPServices.Signals
 {
+#if THEONE_IAP
     using UnityEngine.Purchasing;
+#endif
 
     public class OnIAPPurchaseSuccessSignal
     {
         public string ProductId { get; set; }
+#if THEONE_IAP
         public Product PurchasedProduct { get; set; }
+#endif
     }
 }
