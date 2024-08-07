d9de2b91e94f6e6a63ab1f7741a9ad43efb9df0e
phuongdmp7 <phuongdmp7@gmail.com>
2024-08-07 18:39:50 +0700

    update yandex adapter

--- ServiceImplementation/Configs/Ads/Yandex/YandexDashboard.cs
+++ ServiceImplementation/Configs/Ads/Yandex/YandexDashboard.cs
@@ -6,7 +6,26 @@
     using Cysharp.Threading.Tasks;
     using ServiceImplementation.Configs.Common;
     using Sirenix.OdinInspector;
+    using Sirenix.Utilities;
     using UnityEngine;
+
+    public struct YandexAdNetwork
+    {
+        public const string Adcolony   = "adcolony";
+        public const string Applovin   = "applovin";
+        public const string Bigoads    = "bigoads";
+        public const string Chartboost = "chartboost";
+        public const string Google     = "google";
+        public const string Inmobi     = "inmobi";
+        public const string Ironsource = "ironsource";
+        public const string Mintegral  = "mintegral";
+        public const string Mytarget   = "mytarget";
+        public const string Pangle     = "pangle";
+        public const string Startapp   = "startapp";
+        public const string Tapjoy     = "tapjoy";
+        public const string Unityads   = "unityads";
+        public const string Vungle     = "vungle";
+    }
 
     [Serializable]
     public class YandexDashboard
@@ -33,7 +52,7 @@
             UnityPackageHelper.CopyFile("Assets/YandexMobileAds/Editor/YandexMobileAds.Scripts.asmdef",
                 "Packages/com.gdk.3rd/ServiceImplementation/Configs/Ads/Yandex/YandexMobileAdsEditorAsmDef.json");
 
-            this.UpdateAllNetworkAdapters();
+            this.AdapterInfo.ForEach(pair => this.DownloadNetworkAdapter(pair.Key));
             this.UpdateVersionText();
         }
 
@@ -45,20 +64,20 @@
 
         private Dictionary<string, (bool, string)> AdapterInfo => new()
         {
-            { "adcolony", (this.useAdcolonyAdapter, "MobileadsAdColonyMediationDependencies") },
-            { "applovin", (this.useApplovinAdapter, "MobileadsAppLovinMediationDependencies") },
-            { "bigoads", (this.useBigoadsAdapter, "MobileadsBigoAdsMediationDependencies") },
-            { "chartboost", (this.useChartboostAdapter, "MobileadsChartboostMediationDependencies") },
-            { "google", (this.useGoogleAdapter, "MobileadsGoogleMediationDependencies") },
-            { "inmobi", (this.useInmobiAdapter, "MobileadsInmobiMediationDependencies") },
-            { "ironsource", (this.useIronsourceAdapter, "MobileadsIronSourceMediationDependencies") },
-            { "mintegral", (this.useMintegralAdapter, "MobileadsMintegralMediationDependencies") },
-            { "mytarget", (this.useMytargetAdapter, "MobileadsMytargetMediationDependencies") },
-            { "pangle", (this.usePangleAdapter, "MobileadsPangleMediationDependencies") },
-            { "startapp", (this.useStartappAdapter, "MobileadsStartappMediationDependencies") },
-            { "tapjoy", (this.useTapjoyAdapter, "MobileadsTapjoyMediationDependencies") },
-            { "unityads", (this.useUnityadsAdapter, "MobileadsUnityAdsMediationDependencies") },
-            { "vungle", (this.useVungleAdapter, "MobileadsVungleMediationDependencies") }
+            { YandexAdNetwork.Adcolony, (this.useAdcolonyAdapter, "MobileadsAdColonyMediationDependencies") },
+            { YandexAdNetwork.Applovin, (this.useApplovinAdapter, "MobileadsAppLovinMediationDependencies") },
+            { YandexAdNetwork.Bigoads, (this.useBigoadsAdapter, "MobileadsBigoAdsMediationDependencies") },
+            { YandexAdNetwork.Chartboost, (this.useChartboostAdapter, "MobileadsChartboostMediationDependencies") },
+            { YandexAdNetwork.Google, (this.useGoogleAdapter, "MobileadsGoogleMediationDependencies") },
+            { YandexAdNetwork.Inmobi, (this.useInmobiAdapter, "MobileadsInmobiMediationDependencies") },
+            { YandexAdNetwork.Ironsource, (this.useIronsourceAdapter, "MobileadsIronSourceMediationDependencies") },
+            { YandexAdNetwork.Mintegral, (this.useMintegralAdapter, "MobileadsMintegralMediationDependencies") },
+            { YandexAdNetwork.Mytarget, (this.useMytargetAdapter, "MobileadsMytargetMediationDependencies") },
+            { YandexAdNetwork.Pangle, (this.usePangleAdapter, "MobileadsPangleMediationDependencies") },
+            { YandexAdNetwork.Startapp, (this.useStartappAdapter, "MobileadsStartappMediationDependencies") },
+            { YandexAdNetwork.Tapjoy, (this.useTapjoyAdapter, "MobileadsTapjoyMediationDependencies") },
+            { YandexAdNetwork.Unityads, (this.useUnityadsAdapter, "MobileadsUnityAdsMediationDependencies") },
+            { YandexAdNetwork.Vungle, (this.useVungleAdapter, "MobileadsVungleMediationDependencies") }
         };
 
         public void ResetCacheNetworkAdapters()
@@ -72,17 +91,11 @@
             }
         }
 
-        public void UpdateAllNetworkAdapters()
-        {
-            foreach (var adapter in this.AdapterInfo)
-            {
-                this.DownloadNetworkAdapter(adapter.Key, adapter.Value.Item1, adapter.Value.Item2);
-            }
-        }
-
-        private void DownloadNetworkAdapter(string name, bool isEnable, string xmlName)
+        private void DownloadNetworkAdapter(string name)
         {
 #if UNITY_EDITOR
+            var isEnable = this.AdapterInfo[name].Item1;
+            var xmlName  = this.AdapterInfo[name].Item2;
             if (isEnable)
             {
                 var url = $"https://github.com/yandexmobile/yandex-ads-unity-plugin/raw/master/mobileads-mediation/{name}/mobileads-{name}-mediation-{YandexSdkVersion}.unitypackage";
@@ -92,53 +105,84 @@
             {
                 UnityPackageHelper.DeleteFileIfExists($"Assets/YandexMobileAds/Editor/{xmlName}.xml");
             }
-
 #endif
         }
 
         #region Network Adapters
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters", centerLabel: true)]
+        [SerializeField, OnValueChanged(nameof(OnAdcolonyValueChanged)), BoxGroup("Network Adapters", centerLabel: true)]
         private bool useAdcolonyAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnApplovinValueChanged)), BoxGroup("Network Adapters")]
         private bool useApplovinAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnBigoadsValueChanged)), BoxGroup("Network Adapters")]
         private bool useBigoadsAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnChartboostValueChanged)), BoxGroup("Network Adapters")]
         private bool useChartboostAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnGoogleValueChanged)), BoxGroup("Network Adapters")]
         private bool useGoogleAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnInmobiValueChanged)), BoxGroup("Network Adapters")]
         private bool useInmobiAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnIronsourceValueChanged)), BoxGroup("Network Adapters")]
         private bool useIronsourceAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnMintegralValueChanged)), BoxGroup("Network Adapters")]
         private bool useMintegralAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnMytargetValueChanged)), BoxGroup("Network Adapters")]
         private bool useMytargetAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnPangleValueChanged)), BoxGroup("Network Adapters")]
         private bool usePangleAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnStartappValueChanged)), BoxGroup("Network Adapters")]
         private bool useStartappAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnTapjoyValueChanged)), BoxGroup("Network Adapters")]
         private bool useTapjoyAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnUnityadsValueChanged)), BoxGroup("Network Adapters")]
         private bool useUnityadsAdapter;
 
-        [SerializeField, OnValueChanged(nameof(UpdateAllNetworkAdapters)), BoxGroup("Network Adapters")]
+        [SerializeField, OnValueChanged(nameof(OnVungleValueChanged)), BoxGroup("Network Adapters")]
         private bool useVungleAdapter;
+
+        #endregion
+
+        #region On Network Adapters Changed
+
+        private void OnAdcolonyValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Adcolony);
+
+        private void OnApplovinValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Applovin);
+
+        private void OnBigoadsValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Bigoads);
+
+        private void OnChartboostValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Chartboost);
+
+        private void OnGoogleValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Google);
+
+        private void OnInmobiValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Inmobi);
+
+        private void OnIronsourceValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Ironsource);
+
+        private void OnMintegralValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Mintegral);
+
+        private void OnMytargetValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Mytarget);
+
+        private void OnPangleValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Pangle);
+
+        private void OnStartappValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Startapp);
+
+        private void OnTapjoyValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Tapjoy);
+
+        private void OnUnityadsValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Unityads);
+
+        private void OnVungleValueChanged() => this.DownloadNetworkAdapter(YandexAdNetwork.Vungle);
 
         #endregion
     }
