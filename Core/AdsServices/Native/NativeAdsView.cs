a3091f2603c1dc2ef9cc40b670498763075869fb
mike <duymaibui2k@gmail.com>
2024-08-08 17:56:04 +0700

    update show native ads

--- Core/AdsServices/Native/NativeAdsView.cs
+++ Core/AdsServices/Native/NativeAdsView.cs
@@ -14,19 +14,44 @@ namespace Core.AdsServices.Native
         public Text     callToActionText;
 
         private INativeAdsService nativeAdsService;
+        private Collider[]        colliders;
+        private bool              isEnable;
+        private bool              isInit;
 
 #if ADMOB_NATIVE_ADS && !IMMERSIVE_ADS
+
+        private void Awake()
+        {
+            this.colliders = this.GetComponentsInChildren<Collider>();
+        }
+        
+        public void ShowAds(bool isShow)
+        {
+            if (this.isInit && !this.isEnable && isShow)
+            {
+                this.isEnable = true;
+                this.IntervalCall();
+            }
+            this.isEnable = isShow;
+            foreach (var col in this.colliders)
+            {
+                col.enabled = isShow;
+            }
+        }
+
         public void Init(INativeAdsService nativeAdsService)
         {
             this.nativeAdsService = nativeAdsService;
-
             this.iconImage.gameObject.SetActive(false);
             this.adChoicesImage.gameObject.SetActive(false);
+            this.isInit   = true;
+            this.isEnable = true;
             this.IntervalCall();
         }
 
         private async void IntervalCall()
         {
+            if (!this.isEnable) return;
             if (this == null) return;
             await UniTask.SwitchToMainThread();
             this.nativeAdsService?.DrawNativeAds(this);
