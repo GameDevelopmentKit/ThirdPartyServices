7e4383567a8a7c5b2ed18a9af702eae4322c8976
HungNH <work.hung.nh@gmail.com>
2024-08-23 11:33:10 +0700

    update adjust new version, update tracker

--- ServiceImplementation/Analytic/AdjustAnalyticTracker/ConfigExtension/AdjustConfig.cs
+++ ServiceImplementation/Analytic/AdjustAnalyticTracker/ConfigExtension/AdjustConfig.cs
@@ -20,7 +20,7 @@ namespace Core.AnalyticServices
         {
 #if UNITY_EDITOR
             EditorUtils.SetDefineSymbol(AdjustSymbol, this.isAdjustEnabled);
-            EditorUtils.ModifyPackage(this.isAdjustEnabled, "com.theone.adjust", "https://github.com/The1Studio/adjust.git?path=Assets/Adjust#");
+            EditorUtils.ModifyPackage(this.isAdjustEnabled, "com.adjust.sdk", "https://github.com/The1Studio/adjust.git?path=Assets/Adjust#");
 #endif
         }
         
