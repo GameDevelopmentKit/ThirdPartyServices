daedf8f3900d74c122cabe50adc6b7e7d5b89760
mike <duymaibui2k@gmail.com>
2024-09-30 17:30:17 +0700

    fix turn off native

--- Core/AdsServices/Native/NativeAdsView.cs
+++ Core/AdsServices/Native/NativeAdsView.cs
@@ -29,70 +29,80 @@ namespace Core.AdsServices.Native
         private IDisposable      changeScreenDisposable;
         private IScreenPresenter visibleScreen;
         private IScreenManager   screenManager;
-        
+
+        private Collider[] Colliders
+        {
+            get
+            {
+                this.colliders ??= this.GetComponentsInChildren<Collider>();
+
+                return this.colliders;
+            }
+        }
+
 #if ADMOB_NATIVE_ADS && !IMMERSIVE_ADS
 
-        private void Awake()
-        {
-            this.colliders              = this.GetComponentsInChildren<Collider>();
-            this.screenManager          = this.GetCurrentContainer().Resolve<IScreenManager>();
-            this.changeScreenDisposable = this.screenManager.CurrentActiveScreen.Subscribe(this.OnChangeScreen);
-        }
+    private void Awake()
+    {
+        this.screenManager          = this.GetCurrentContainer().Resolve<IScreenManager>();
+        this.changeScreenDisposable = this.screenManager.CurrentActiveScreen.Subscribe(this.OnChangeScreen);
+    }
 
-        private void OnDestroy()
-        {
-            this.ShowAds(false);
-            this.changeScreenDisposable?.Dispose();
-        }
+    private void OnDestroy()
+    {
+        this.ShowAds(false);
+        this.changeScreenDisposable?.Dispose();
+    }
 
-        private void OnChangeScreen(IScreenPresenter screenPresenter)
-        {
-            if (this.visibleScreen == null) return;
-            this.ShowAds(this.visibleScreen == screenPresenter);
-        }
+    private void OnChangeScreen(IScreenPresenter screenPresenter)
+    {
+        if (this.visibleScreen == null) return;
+        this.ShowAds(this.visibleScreen == screenPresenter);
+    }
 
-        public void ShowAds(bool isShow)
+    public void ShowAds(bool isShow)
+    {
+        this.nonAdsHolder.SetActive(!isShow);
+        this.adsHolder.SetActive(isShow);
+        if (this.isInit && !this.isEnable && isShow)
         {
-            this.nonAdsHolder.SetActive(!isShow);
-            this.adsHolder.SetActive(isShow);
-            if (this.isInit && !this.isEnable && isShow)
-            {
-                this.isEnable = true;
-                this.IntervalCall();
-            }
-
-            this.isEnable = isShow;
-            foreach (var col in this.colliders)
-            {
-                col.enabled = isShow;
-            }
-        }
-
-        public void BindVisibleScreen(IScreenPresenter screenPresenter)
-        {
-            this.visibleScreen = screenPresenter;
-        }
-
-        public void Init(INativeAdsService nativeAdsService)
-        {
-            this.nativeAdsService = nativeAdsService;
-            this.iconImage.gameObject.SetActive(false);
-            this.adChoicesImage.gameObject.SetActive(false);
-            this.isInit   = true;
             this.isEnable = true;
             this.IntervalCall();
-            this.ShowAds(true);
         }
 
-        private async void IntervalCall()
+        this.isEnable = isShow;
+        foreach (var col in this.Colliders)
         {
-            if (!this.isEnable) return;
-            if (this == null) return;
-            await UniTask.SwitchToMainThread();
-            this.nativeAdsService?.DrawNativeAds(this);
-            await UniTask.Delay(TimeSpan.FromSeconds(1));
-            this.IntervalCall();
+            col.enabled = isShow;
         }
-#endif
     }
+
+    public void BindVisibleScreen(IScreenPresenter screenPresenter)
+    {
+        this.visibleScreen = screenPresenter;
+    }
+
+    public void Init(INativeAdsService nativeAdsService)
+    {
+        this.nativeAdsService = nativeAdsService;
+        this.iconImage.gameObject.SetActive(false);
+        this.adChoicesImage.gameObject.SetActive(false);
+        this.isInit   = true;
+        this.isEnable = true;
+        this.IntervalCall();
+        this.ShowAds(true);
+    }
+
+    private async void IntervalCall()
+    {
+        if (!this.isEnable) return;
+        if (this == null) return;
+        await UniTask.SwitchToMainThread();
+        this.nativeAdsService?.DrawNativeAds(this);
+        await UniTask.Delay(TimeSpan.FromSeconds(1));
+        this.IntervalCall();
+    }
+#endif
+}
+
 }
