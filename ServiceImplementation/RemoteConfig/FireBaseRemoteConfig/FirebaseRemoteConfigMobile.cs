e40d94ccdfefa6449ed28aec21893ed3de469eec
Hoàng Anh Tú <tuha263@gmail.com>
2024-10-01 11:33:24 +0700

    Implement packages for remote config

--- ServiceImplementation/RemoteConfig/FireBaseRemoteConfig/FirebaseRemoteConfigMobile.cs
+++ ServiceImplementation/RemoteConfig/FireBaseRemoteConfig/FirebaseRemoteConfigMobile.cs
@@ -15,7 +15,7 @@ namespace ServiceImplementation.FireBaseRemoteConfig
     /// <summary>
     /// We need to use MonoBehaviour to use Firebase Remote Config
     /// </summary>
-    internal class FirebaseRemoteConfigMobile : IRemoteConfig, IInitializable
+    public class FirebaseRemoteConfigMobile : IRemoteConfig, IInitializable
     {
         private readonly ILogService logger;
         private readonly SignalBus   signalBus;
