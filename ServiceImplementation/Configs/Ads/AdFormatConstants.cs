15dbc00167a7a9e162a76034fbd13c09af5e28b1
hailuu <hoanghai.luu.71@gmail.com>
2024-10-03 17:46:47 +0700

    chore: normalization ads format for Applovin and Admob

---
+++ ServiceImplementation/Configs/Ads/AdFormatConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceImplementation
+{
+
+    public class AdFormatConstants
+    {
+        public const string Banner            = "BANNER";
+        public const string Interstitial      = "INTER";
+        public const string Rewarded          = "REWARDED";
+        public const string AppOpen           = "AOA";
+        public const string Native            = "NATIVE";
+        public const string MREC              = "MREC";
+        public const string CollapsibleBanner = "COLLAPSIBLE_BANNER";
+    }
+
+}
