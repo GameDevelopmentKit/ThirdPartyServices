6adb9a6f3f37d74d69c5f67d0422284ebae37a9c
ducyb294 <ducyb2942@gmail.com>
2024-10-28 20:24:33 +0700

    test mrec

--- ServiceImplementation/AdsServices/AdScreenPositionExtension.cs
+++ ServiceImplementation/AdsServices/AdScreenPositionExtension.cs
@@ -6,7 +6,7 @@
     public static class AdScreenPositionExtension
     {
         private const int MREC_WIDTH  = 300;
-        private const int MREC_HEIGHT = 350;
+        private const int MREC_HEIGHT = 300;
         
         public static AdScreenPosition CanvasToUnityCoordinateSystem(this AdScreenPosition adScreenPosition)
         {
