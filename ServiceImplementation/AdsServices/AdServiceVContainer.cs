af7ee4b98d351796ed8d7e8660eae58e3fe36ea2
phuongdmp7 <83566201+phuongdmp7@users.noreply.github.com>
2024-10-28 15:23:23 +0700

    ignore show aoa after request att (#222)
    * ignore show aoa after request att
    
    * add this keyword
    
    * update name
    
    * update register

--- ServiceImplementation/AdsServices/AdServiceVContainer.cs
+++ ServiceImplementation/AdsServices/AdServiceVContainer.cs
@@ -75,7 +75,7 @@ namespace ServiceImplementation.AdsServices
             builder.Register<PreloadAdService>(Lifetime.Singleton).AsImplementedInterfaces();
             typeof(IAdRevenueTracker).GetDerivedTypes().ForEach(type => builder.Register(type, Lifetime.Singleton).AsImplementedInterfaces());
 
-            builder.Register<AppTrackingServices>(Lifetime.Singleton).AsImplementedInterfaces();
+            builder.Register<AppTrackingServices>(Lifetime.Singleton).AsInterfacesAndSelf();
             #if ADMOB
             builder.Register<UmpConsentInformation>(Lifetime.Singleton).AsImplementedInterfaces();
             #else
@@ -137,6 +137,9 @@ namespace ServiceImplementation.AdsServices
             builder.DeclareSignal<AppOpenClickedSignal>();
 
             builder.DeclareSignal<AppStateChangeSignal>();
+
+            builder.DeclareSignal<AttDisplayedSignal>();
+            builder.DeclareSignal<AttClosedSignal>();
 
             #endregion
         }
--- ServiceImplementation/AdsServices/ConsentInformation/AppTrackingServices.cs
+++ ServiceImplementation/AdsServices/ConsentInformation/AppTrackingServices.cs
@@ -2,6 +2,8 @@ namespace ServiceImplementation.AdsServices.ConsentInformation
 {
     using Cysharp.Threading.Tasks;
     using GameFoundation.DI;
+    using GameFoundation.Signals;
+    using ServiceImplementation.AdsServices.Signal;
     using ServiceImplementation.Configs;
     using UnityEngine.Scripting;
 
@@ -10,26 +12,30 @@ namespace ServiceImplementation.AdsServices.ConsentInformation
         protected virtual int DelayRequestTrackingMillisecond { get; set; } = 100;
 
         private readonly ThirdPartiesConfig thirdPartiesConfig;
+        private readonly SignalBus          signalBus;
 
         [Preserve]
-        public AppTrackingServices(ThirdPartiesConfig thirdPartiesConfig)
+        public AppTrackingServices(ThirdPartiesConfig thirdPartiesConfig, SignalBus signalBus)
         {
             this.thirdPartiesConfig = thirdPartiesConfig;
+            this.signalBus          = signalBus;
         }
 
         public async void Initialize()
         {
             await UniTask.Delay(this.DelayRequestTrackingMillisecond);
-            if (this.thirdPartiesConfig.AdSettings.autoRequestATT) await RequestTracking();
+            if (this.thirdPartiesConfig.AdSettings.autoRequestATT) await this.RequestTracking();
         }
 
-        public static async UniTask RequestTracking()
+        public async UniTask RequestTracking()
         {
             if (AttHelper.IsRequestTrackingComplete()) return;
 
             #if UNITY_IOS
+            this.signalBus.Fire(new AttDisplayedSignal());
             Unity.Advertisement.IosSupport.ATTrackingStatusBinding.RequestAuthorizationTracking();
             await UniTask.WaitUntil(AttHelper.IsRequestTrackingComplete);
+            this.signalBus.Fire(new AttClosedSignal());
             #endif
         }
     }
