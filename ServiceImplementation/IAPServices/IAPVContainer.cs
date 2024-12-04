7288970e57d50a5ec21aa93c40df09bdd3cbd0f6
tuha <tuha263@gmail.com>
2024-12-04 20:29:30 +0700

    Fix IAP for editor

--- ServiceImplementation/IAPServices/IAPVContainer.cs
+++ ServiceImplementation/IAPServices/IAPVContainer.cs
@@ -10,7 +10,7 @@ namespace ServiceImplementation.IAPServices
     {
         public static void RegisterIAPService(this IContainerBuilder builder)
         {
-            #if THEONE_IAP
+            #if THEONE_IAP && !UNITY_EDITOR
             builder.Register<UnityIapServices>(Lifetime.Singleton).AsImplementedInterfaces();
             #else
             builder.Register<DummyIapServices>(Lifetime.Singleton).AsImplementedInterfaces();
