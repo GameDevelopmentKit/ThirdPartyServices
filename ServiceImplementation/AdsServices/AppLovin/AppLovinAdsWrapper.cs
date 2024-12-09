01ed1f853542e9a5cb55dce241556edd07fc0bd5
Đức Lê <ducyb2942@gmail.com>
2024-12-09 14:40:29 +0700

    update applovin

--- ServiceImplementation/AdsServices/AppLovin/AppLovinAdsWrapper.cs
+++ ServiceImplementation/AdsServices/AppLovin/AppLovinAdsWrapper.cs
@@ -143,15 +143,27 @@ namespace ServiceImplementation.AdsServices.AppLovin
             MaxSdk.ShowMRec(adsId);
         }
 
-        public bool IsMRECReady(string placement, AdScreenPosition position)
+        public bool IsMRECReady(string placement, AdScreenPosition position, AdScreenPosition offset)
         {
             return this.AppLovinSetting.MRECAdIds.TryGetValue(AdPlacement.PlacementWithName(placement), out _);
         }
 
-        public void HideMREC(string placement, AdScreenPosition position)
+        public void HideMREC(string placement)
         {
             var adsId = this.AppLovinSetting.MRECAdIds[AdPlacement.PlacementWithName(placement)].Id;
             this.InternalHideMREC(adsId);
+        }
+        
+        public void DestroyMREC(string placement)
+        {
+            var adsId = this.AppLovinSetting.MRECAdIds[AdPlacement.PlacementWithName(placement)].Id;
+            this.InternalDestroyMREC(adsId);
+        }
+        
+        public void InternalDestroyMREC(string adUnitId)
+        {
+            this.OnMRecAdDismissed(adUnitId);
+            MaxSdk.DestroyMRec(adUnitId);
         }
 
         public void InternalHideMREC(string adUnitId)
@@ -163,7 +175,6 @@ namespace ServiceImplementation.AdsServices.AppLovin
         public void StopMRECAutoRefresh(string adUnitId) { MaxSdk.StopMRecAutoRefresh(adUnitId); }
 
         public void StartMRECAutoRefresh(string adUnitId) { MaxSdk.StartMRecAutoRefresh(adUnitId); }
-
 
         public void LoadMREC(string adUnitId) { MaxSdk.LoadMRec(adUnitId); }
 
