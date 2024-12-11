07571d878b60b280cd2af866633d058d47e7bc6e
HungNH <work.hung.nh@gmail.com>
2024-12-11 15:48:55 +0700

    debug fetch rmcf

--- ServiceImplementation/Configs/Ads/AdServicesConfig.cs
+++ ServiceImplementation/Configs/Ads/AdServicesConfig.cs
@@ -4,6 +4,7 @@ namespace ServiceImplementation.Configs.Ads
     using GameFoundation.DI;
     using GameFoundation.Signals;
     using ServiceImplementation.FireBaseRemoteConfig;
+    using UnityEngine;
     using UnityEngine.Scripting;
 
     public class AdServicesConfig : IInitializable, IDisposable
@@ -156,6 +157,8 @@ namespace ServiceImplementation.Configs.Ads
             this.EnableRewardedAd             = RemoteConfigHelpers.GetBoolRemoteValue(this.remoteConfig, this.remoteConfigSetting, RemoteConfigKey.EnableRewardedAD) && this.EnableAds;
             this.EnableRewardedInterstitialAd = RemoteConfigHelpers.GetBoolRemoteValue(this.remoteConfig, this.remoteConfigSetting, RemoteConfigKey.EnableRewardedInterstitialAD) && this.EnableAds;
             this.EnableNativeAd               = RemoteConfigHelpers.GetBoolRemoteValue(this.remoteConfig, this.remoteConfigSetting, RemoteConfigKey.EnableNativeAD) && this.EnableAds;
+            
+            Debug.Log($"onelog: fetch rmcf enableads {this.EnableAds}  ");
             #if THEONE_COLLAPSIBLE_BANNER
             this.EnableCollapsibleBanner = RemoteConfigHelpers.GetBoolRemoteValue(this.remoteConfig, this.remoteConfigSetting, RemoteConfigKey.EnableCollapsibleBanner) && this.EnableAds;
             #endif
