4fc6f558ee9cb58cf2589c4082e9866952c60d6a
ngnqu <ngnquocuong@gmail.com>
2024-12-25 14:27:26 +0700

    check re-creat mrec

--- ServiceImplementation/AdsServices/AppLovin/AppLovinAdsWrapper.cs
+++ ServiceImplementation/AdsServices/AppLovin/AppLovinAdsWrapper.cs
@@ -162,15 +162,16 @@ namespace ServiceImplementation.AdsServices.AppLovin
             var adsId = this.AppLovinSetting.MRECAdIds[AdPlacement.PlacementWithName(placement)].Id;
             this.InternalHideMREC(adsId);
         }
-        
+
         public void DestroyMREC(string placement)
         {
             var adsId = this.AppLovinSetting.MRECAdIds[AdPlacement.PlacementWithName(placement)].Id;
             this.InternalDestroyMREC(adsId);
         }
-        
+
         public void InternalDestroyMREC(string adUnitId)
         {
+            this.idMRecCreating.Remove(adUnitId);
             this.OnMRecAdDismissed(adUnitId);
             MaxSdk.DestroyMRec(adUnitId);
         }
