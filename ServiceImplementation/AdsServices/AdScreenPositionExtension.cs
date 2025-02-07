ea6beb7906d401282c243b1e4831754e19da40ce
phuongdmp <83566201+phuongdmp7@users.noreply.github.com>
2025-02-07 14:29:42 +0700

    fix mrec ios position (#276)
    * fix mrec ios position
    
    * add screen scale ios
    
    * using InteropServices
    
    * add log
    
    * update test
    
    * fix

--- ServiceImplementation/AdsServices/AdScreenPositionExtension.cs
+++ ServiceImplementation/AdsServices/AdScreenPositionExtension.cs
@@ -7,22 +7,22 @@
     {
         private const int MREC_WIDTH  = 300;
         private const int MREC_HEIGHT = 250;
-        
+
         public static AdScreenPosition CanvasToUnityCoordinateSystem(this AdScreenPosition adScreenPosition)
         {
             return new AdScreenPosition(adScreenPosition.x, Mathf.Abs(adScreenPosition.y - Screen.safeArea.height));
         }
-        
+
         public static AdScreenPosition FlipY(this AdScreenPosition adScreenPosition)
         {
-            return new AdScreenPosition(PixelToDp(adScreenPosition.x), - PixelToDp(adScreenPosition.y));
+            return new AdScreenPosition(PixelToDp(adScreenPosition.x), -PixelToDp(adScreenPosition.y));
         }
 
         #if APPLOVIN
         public static AdScreenPosition ToApplovinPosition(this AdScreenPosition adScreenPosition)
         {
             // calculate in canvas coordinate system
-            var density    = MaxSdkUtils.GetScreenDensity();
+            var density = MaxSdkUtils.GetScreenDensity();
             var connerPosX = adScreenPosition.x - MREC_WIDTH  * density * (adScreenPosition.x / Screen.safeArea.width);
             var connerPosY = adScreenPosition.y - MREC_HEIGHT * density * (adScreenPosition.y / Screen.safeArea.height);
 
@@ -44,7 +44,19 @@
             return new AdScreenPosition(connerPosX, connerPosY);
         }
         #endif
-        
-        public static float PixelToDp(float pixel) { return pixel * 160f / Screen.dpi; }
+
+        public static float PixelToDp(float pixel)
+        {
+            return pixel * 160f / (Screen.dpi * GetScaleFactor());
+        }
+
+        private static float GetScaleFactor()
+        {
+            #if UNITY_IOS
+            return Screen.width > 1500 ? Screen.width / 1155f : 1; // after testing, 1155 is the best divider value for ipad
+            #else
+            return 1f;
+            #endif
+        }
     }
 }
