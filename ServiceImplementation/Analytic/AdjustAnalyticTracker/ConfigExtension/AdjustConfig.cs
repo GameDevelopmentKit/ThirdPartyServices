eb2e44e28383f0f4be2477f386f029ea4efc746f
phuongdmp <phuongdmp7@gmail.com>
2025-02-07 10:01:52 +0700

    phuongdmp/native/change-adjust-back-company-repo

--- ServiceImplementation/Analytic/AdjustAnalyticTracker/ConfigExtension/AdjustConfig.cs
+++ ServiceImplementation/Analytic/AdjustAnalyticTracker/ConfigExtension/AdjustConfig.cs
@@ -19,7 +19,7 @@ namespace Core.AnalyticServices
         {
             #if UNITY_EDITOR
             EditorUtils.SetDefineSymbol(AdjustSymbol, this.isAdjustEnabled);
-            EditorUtils.ModifyPackage(this.isAdjustEnabled, "com.adjust.sdk", "5.0.6");
+            EditorUtils.ModifyPackage(this.isAdjustEnabled, "com.adjust.sdk", "https://github.com/The1Studio/adjust.git?path=Assets/Adjust#");
             #endif
         }
 
