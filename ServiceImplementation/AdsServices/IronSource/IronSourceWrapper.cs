66954fff2a87b2caa30711385880bdd949a2418a
phuongdmp <83566201+phuongdmp7@users.noreply.github.com>
2025-02-20 13:35:26 +0700

    fix banner show multiple time ironsource (#279)
    * fix banner show multiple time
    
    * check null

--- ServiceImplementation/AdsServices/IronSource/IronSourceWrapper.cs
+++ ServiceImplementation/AdsServices/IronSource/IronSourceWrapper.cs
@@ -434,6 +434,8 @@ namespace ServiceImplementation.AdsServices.EasyMobile
                     return;
                 }
 
+                this.DestroyBannerAd(); // unlisten old banner
+
                 var position = bannerAdsPosition switch
                                {
                                    BannerAdsPosition.Top => LevelPlayBannerPosition.TopCenter,
@@ -474,13 +476,14 @@ namespace ServiceImplementation.AdsServices.EasyMobile
 
         public void DestroyBannerAd()
         {
+            this.isLoadedBanner = false;
+            if(this.bannerAd == null) return;
             this.bannerAd.OnAdLoaded     -= this.OnBannerLoaded;
             this.bannerAd.OnAdLoadFailed -= this.OnBannerLoadFailed;
             this.bannerAd.OnAdClicked    -= this.OnBannerClicked;
             this.bannerAd.OnAdDisplayed  -= this.BannerOnAdScreenPresentedEvent;
             this.bannerAd.OnAdCollapsed  -= this.BannerOnAdScreenDismissedEvent;
             this.bannerAd.DestroyAd();
-            this.isLoadedBanner = false;
         }
 
         public bool IsInterstitialAdReady(string place)
