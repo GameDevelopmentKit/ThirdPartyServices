b713f4d76777c1d61608b3b8a68d7958a3481d6f
Dung <dungnguyen260103@gmail.com>
2025-03-20 14:47:49 +0700

    can show banner ads

--- ServiceImplementation/AdsServices/AppLovin/AppLovinAdsWrapper.cs
+++ ServiceImplementation/AdsServices/AppLovin/AppLovinAdsWrapper.cs
@@ -59,12 +59,12 @@ namespace ServiceImplementation.AdsServices.AppLovin
 #if THEONE_ADS_DEBUG
             MaxSdk.SetCreativeDebuggerEnabled(true);
 #endif
-            
+
 #if BRAVESTARS
             MaxSdk.SetHasUserConsent(true);
             MaxSdk.SetDoNotSell(false);
 #endif
-            
+
             MaxSdk.SetSdkKey(this.AppLovinSetting.SDKKey);
 #if COLUMBUS
             ColumbusSdk.SetGDPRConsent(false);
@@ -289,6 +289,28 @@ namespace ServiceImplementation.AdsServices.AppLovin
             if (!this.IsBannerPlacementReady(adPlacement.Name, out var id)) return;
             MaxSdk.DestroyBanner(id);
             this.placementToBanner.Remove(adPlacement);
+        }
+
+        public bool IsBannerAdReady()
+        {
+            if (!this.isInit)
+            {
+                this.logService.Log("AppLovin SDK is not initialized.");
+                return false;
+            }
+
+            if (!this.IsBannerPlacementReady(AdPlacement.Default.Name, out var id))
+            {
+                this.logService.Log("Banner ad placement is not ready.");
+                return false;
+            }
+            if (this.IsRemoveAds())
+            {
+                this.logService.Log("Remove ads is enabled.");
+                return false;
+            }
+
+            return true;
         }
 
         #endregion
