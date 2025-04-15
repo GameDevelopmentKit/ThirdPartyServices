798c0fb7034ccceadde58cf6f1c192d8ba742a63
Đức Lê <ducyb2942@gmail.com>
2025-04-15 11:19:19 +0700

    IS ad format constants

--- ServiceImplementation/Configs/Ads/AdFormatConstants.cs
+++ ServiceImplementation/Configs/Ads/AdFormatConstants.cs
@@ -2,18 +2,19 @@
 {
     public class AdFormatConstants
     {
-        #if IRONSOURCE
-        public const string Banner            = "banner";
-        public const string Interstitial      = "interstitial";
-        public const string Rewarded          = "rewarded_video";
-        #else
         public const string Banner            = "BANNER";
         public const string Interstitial      = "INTER";
         public const string Rewarded          = "REWARDED";
-        #endif
         public const string AppOpen           = "AOA";
         public const string Native            = "NATIVE";
         public const string MREC              = "MREC";
         public const string CollapsibleBanner = "COLLAPSIBLE_BANNER";
+    }
+
+    public class IronSourceAdFormatConstants
+    {
+        public const string Banner       = "banner";
+        public const string Interstitial = "interstitial";
+        public const string Rewarded     = "rewarded_video";
     }
 }
