62ad985c1ab07457ca89244e4f2cb2bb97852c0c
hoanghai145 <hoangthanhhai1308@gmail.com>
2025-04-30 14:35:33 +0700

    Update log dummy iap

--- ServiceImplementation/IAPServices/DummyIapServices.cs
+++ ServiceImplementation/IAPServices/DummyIapServices.cs
@@ -2,18 +2,31 @@ namespace ServiceImplementation.IAPServices
 {
     using System;
     using System.Collections.Generic;
-    using Core.AdsServices;
-    using Zenject;
+    using GameFoundation.Scripts.Utilities.LogService;
 
     public class DummyIapServices : IIapServices
     {
+        private readonly ILogService logger;
+
+        public DummyIapServices(ILogService logger) { this.logger = logger; }
+
         public void InitIapServices(Dictionary<string, IAPModel> iapPack, string environment = "production") { }
 
-        public void BuyProductID(string productId, Action<string> onComplete = null, Action<string> onFailed = null) { onComplete?.Invoke(productId); }
+        public void BuyProductID(string productId, Action<string> onComplete = null, Action<string> onFailed = null)
+        {
+            this.logger.Log($"DummyIapServices: BuyProductID {productId}");
+            onComplete?.Invoke(productId);
+        }
 
-        public string      GetPriceById(string productId, string defaultPrice) { return $"$2.99"; }
-        public void        RestorePurchases(Action onComplete)                 { onComplete?.Invoke(); }
-        public bool        IsProductOwned(string productId)                    { return true; }
-        public ProductData GetProductData(string productId)                    { return new ProductData(); }
+        public string GetPriceById(string productId, string defaultPrice) { return $"$2.99"; }
+
+        public void RestorePurchases(Action onComplete)
+        {
+            this.logger.Log("DummyIapServices: RestorePurchases");
+            onComplete?.Invoke();
+        }
+
+        public bool        IsProductOwned(string productId) { return true; }
+        public ProductData GetProductData(string productId) { return new ProductData(); }
     }
 }
